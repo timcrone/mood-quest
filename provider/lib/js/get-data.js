b014661dc3126a97ce18649ac0fd5902c33b36de
Tim Crone <tcrone3@gatech.edu>
2020-11-08 21:59:04 -0500

    Fix write issue with provider

--- provider/lib/js/get-data.js
+++ provider/lib/js/get-data.js
@@ -46,14 +46,14 @@ function displayPatient(pt) {
   document.getElementById('dob').innerHTML = pt.birthDate;
 }
 
-// Gets the BDRS questionnaire reference
+// Gets the BDRS questionnaire reference; returns the FHIR ID for the questionnaire
 async function checkQuestionnaire(client) {
     let result = await getQuestionnaire(client);
     if (result.total > 0) {
-        return result.entry[0].resource;
+        return result.entry[0].resource.id;
     }
     result = await client.create(defaultQuestionnaire());
-    return result;
+    return result.entry[0].resource.id;
 }
 
 function getQuestionnaire(client) {
@@ -102,10 +102,7 @@ function _addResponseBody(client) {
 
     console.log("starting write");
     let completed = true;
-    getQuestionnaire(client).then((result) => {
-        console.log(result);
-        quest = result.entry[0].resource.id;
-
+    checkQuestionnaire(client).then((quest) => {
         let items = [];
         let val = 0;
         for (let i = 1; i < 21; i++) {
