2af2ed266d20cbb1813827eba49e3aa0f929e47b
Tim Crone <tcrone3@gatech.edu>
2020-11-23 23:16:41 -0500

    Mood Zoom begins at 1

--- provider/lib/js/display-chart.js
+++ provider/lib/js/display-chart.js
@@ -258,6 +258,7 @@ async function buildChart(client) {
                             stacked: true,
                             ticks: {
                                 // beginAtZero: true
+                                min: 1
                             }
                         }]
                     }
